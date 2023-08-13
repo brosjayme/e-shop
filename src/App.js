bf1a4c23e6a6822b65329be408cb5165e4166116
unknown <brosjayme@gmail.com>
2023-08-13 17:43:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-logo"></div>
+
+      <div className="header-searchbar"></div>
+      <div className="header-nav">
+        <div className="nav-item">
+          <span className="nav-itemlineOne">Hello Guests</span>
+          <span className="nav-itemlinetwo">Sign In</span>
+        </div>
+        <div className="nav-item">
+          <span className="nav-itemLineOne">Your</span>
+          <span className="nav-itemLineTwo">Shop</span>
+        </div>
+        <div className="nav-item">
+          <ShoppingBasketIcon fontSize="large" />
+          <span className="nav-itemLineTwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
